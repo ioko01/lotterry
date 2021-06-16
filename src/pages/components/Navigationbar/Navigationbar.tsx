ac08bea58b2e42f4f9c0a3e2d13add7ad86a382a
unknown <ioko.peezaza@gmail.com>
2021-06-16 18:19:39 +0700

    2

--- src/pages/components/Navigationbar/Navigationbar.tsx
+++ src/pages/components/Navigationbar/Navigationbar.tsx
@@ -105,42 +105,46 @@ const Navigationbar = (props: Props) => {
     };
 
     const link = pages.map((page, index) => {
-        const findRoles = page["role"].find((res) => {
-            return isLogin.role === res;
+        page.role.map((res) => {
+            if (isLogin.role === res) {
+                return (
+                    <ActiveLink
+                        key={index}
+                        href={page.path}
+                        as={page.as}
+                        underline="none"
+                    >
+                        <List>
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                paddingX={2}
+                            >
+                                {page.name === "หน้าหลัก" ? (
+                                    <HomeIcon titleAccess={page.name} />
+                                ) : page.name === "ประวัติการซื้อ" ? (
+                                    <HistoryIcon titleAccess={page.name} />
+                                ) : page.name === "บัญชีการเงิน" ? (
+                                    <PaymentIcon titleAccess={page.name} />
+                                ) : page.name === "ข้อมูลผู้ใช้" ? (
+                                    <PersonIcon titleAccess={page.name} />
+                                ) : page.name === "เพิ่มลูกทีม" ? (
+                                    <PersonAddIcon titleAccess={page.name} />
+                                ) : page.name === "เพิ่มล็อตเตอรี่" ? (
+                                    <AddShoppingCartIcon
+                                        titleAccess={page.name}
+                                    />
+                                ) : page.name === "สลากทั้งหมด" ? (
+                                    <NoteIcon titleAccess={page.name} />
+                                ) : null}
+
+                                <div>&nbsp;{page.name}</div>
+                            </Box>
+                        </List>
+                    </ActiveLink>
+                );
+            }
         });
-
-        if (findRoles) {
-            return (
-                <ActiveLink
-                    key={index}
-                    href={page.path}
-                    as={page.as}
-                    underline="none"
-                >
-                    <List>
-                        <Box display="flex" alignItems="center" paddingX={2}>
-                            {page.name === "หน้าหลัก" ? (
-                                <HomeIcon titleAccess={page.name} />
-                            ) : page.name === "ประวัติการซื้อ" ? (
-                                <HistoryIcon titleAccess={page.name} />
-                            ) : page.name === "บัญชีการเงิน" ? (
-                                <PaymentIcon titleAccess={page.name} />
-                            ) : page.name === "ข้อมูลผู้ใช้" ? (
-                                <PersonIcon titleAccess={page.name} />
-                            ) : page.name === "เพิ่มลูกทีม" ? (
-                                <PersonAddIcon titleAccess={page.name} />
-                            ) : page.name === "เพิ่มล็อตเตอรี่" ? (
-                                <AddShoppingCartIcon titleAccess={page.name} />
-                            ) : page.name === "สลากทั้งหมด" ? (
-                                <NoteIcon titleAccess={page.name} />
-                            ) : null}
-
-                            <div>&nbsp;{page.name}</div>
-                        </Box>
-                    </List>
-                </ActiveLink>
-            );
-        }
     });
 
     const drawer = (
--- src/pages/components/Navigationbar/SecondNavigationbar.tsx
+++ src/pages/components/Navigationbar/SecondNavigationbar.tsx
@@ -86,48 +86,46 @@ const SecondNavigationbar = () => {
     };
 
     const link = pages.map((page, index) => {
-        const findRoles = page["role"].find((res) => {
-            return isLogin.role === res;
-        });
+        page.role.map((res) => {
+            if (isLogin.role === res) {
+                return (
+                    <ActiveLink
+                        key={index}
+                        href={page.path}
+                        as={page.as}
+                        underline="none"
+                    >
+                        <div className={classes.layoutDesktopAndMobile}>
+                            {page.name === "หน้าหลัก" ? (
+                                <HomeIcon
+                                    className={classes.sectionMobile}
+                                    titleAccess={page.name}
+                                />
+                            ) : page.name === "ประวัติการซื้อ" ? (
+                                <HistoryIcon
+                                    className={classes.sectionMobile}
+                                    titleAccess={page.name}
+                                />
+                            ) : page.name === "บัญชีการเงิน" ? (
+                                <PaymentIcon
+                                    className={classes.sectionMobile}
+                                    titleAccess={page.name}
+                                />
+                            ) : page.name === "ข้อมูลผู้ใช้" ? (
+                                <PersonIcon
+                                    className={classes.sectionMobile}
+                                    titleAccess={page.name}
+                                />
+                            ) : null}
 
-        if (findRoles) {
-            return (
-                <ActiveLink
-                    key={index}
-                    href={page.path}
-                    as={page.as}
-                    underline="none"
-                >
-                    <div className={classes.layoutDesktopAndMobile}>
-                        {page.name === "หน้าหลัก" ? (
-                            <HomeIcon
-                                className={classes.sectionMobile}
-                                titleAccess={page.name}
-                            />
-                        ) : page.name === "ประวัติการซื้อ" ? (
-                            <HistoryIcon
-                                className={classes.sectionMobile}
-                                titleAccess={page.name}
-                            />
-                        ) : page.name === "บัญชีการเงิน" ? (
-                            <PaymentIcon
-                                className={classes.sectionMobile}
-                                titleAccess={page.name}
-                            />
-                        ) : page.name === "ข้อมูลผู้ใช้" ? (
-                            <PersonIcon
-                                className={classes.sectionMobile}
-                                titleAccess={page.name}
-                            />
-                        ) : null}
-
-                        <div className={classes.sectionDesktop}>
-                            &nbsp;{page.name}
+                            <div className={classes.sectionDesktop}>
+                                &nbsp;{page.name}
+                            </div>
                         </div>
-                    </div>
-                </ActiveLink>
-            );
-        }
+                    </ActiveLink>
+                );
+            }
+        });
     });
 
     return (
