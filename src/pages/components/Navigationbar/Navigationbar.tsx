b9f1bee83e008fda05d09ffe631dd28d791d9eef
unknown <ioko.peezaza@gmail.com>
2021-06-16 17:28:09 +0700

    2

--- src/pages/components/Navigationbar/Navigationbar.tsx
+++ src/pages/components/Navigationbar/Navigationbar.tsx
@@ -104,38 +104,40 @@ const Navigationbar = (props: Props) => {
         }
     };
 
-    const link = pages.forEach((page, index) =>
-        page.role.find((res) => isLogin.role === res) ? (
-            <ActiveLink
-                key={index}
-                href={page.path}
-                as={page.as}
-                underline="none"
-            >
-                <List>
-                    <Box display="flex" alignItems="center" paddingX={2}>
-                        {page.name === "หน้าหลัก" ? (
-                            <HomeIcon titleAccess={page.name} />
-                        ) : page.name === "ประวัติการซื้อ" ? (
-                            <HistoryIcon titleAccess={page.name} />
-                        ) : page.name === "บัญชีการเงิน" ? (
-                            <PaymentIcon titleAccess={page.name} />
-                        ) : page.name === "ข้อมูลผู้ใช้" ? (
-                            <PersonIcon titleAccess={page.name} />
-                        ) : page.name === "เพิ่มลูกทีม" ? (
-                            <PersonAddIcon titleAccess={page.name} />
-                        ) : page.name === "เพิ่มล็อตเตอรี่" ? (
-                            <AddShoppingCartIcon titleAccess={page.name} />
-                        ) : page.name === "สลากทั้งหมด" ? (
-                            <NoteIcon titleAccess={page.name} />
-                        ) : null}
+    const link = pages.map((page, index) => {
+        if (page.role.find((res) => isLogin.role === res)) {
+            return (
+                <ActiveLink
+                    key={index}
+                    href={page.path}
+                    as={page.as}
+                    underline="none"
+                >
+                    <List>
+                        <Box display="flex" alignItems="center" paddingX={2}>
+                            {page.name === "หน้าหลัก" ? (
+                                <HomeIcon titleAccess={page.name} />
+                            ) : page.name === "ประวัติการซื้อ" ? (
+                                <HistoryIcon titleAccess={page.name} />
+                            ) : page.name === "บัญชีการเงิน" ? (
+                                <PaymentIcon titleAccess={page.name} />
+                            ) : page.name === "ข้อมูลผู้ใช้" ? (
+                                <PersonIcon titleAccess={page.name} />
+                            ) : page.name === "เพิ่มลูกทีม" ? (
+                                <PersonAddIcon titleAccess={page.name} />
+                            ) : page.name === "เพิ่มล็อตเตอรี่" ? (
+                                <AddShoppingCartIcon titleAccess={page.name} />
+                            ) : page.name === "สลากทั้งหมด" ? (
+                                <NoteIcon titleAccess={page.name} />
+                            ) : null}
 
-                        <div>&nbsp;{page.name}</div>
-                    </Box>
-                </List>
-            </ActiveLink>
-        ) : null
-    );
+                            <div>&nbsp;{page.name}</div>
+                        </Box>
+                    </List>
+                </ActiveLink>
+            );
+        }
+    });
 
     const drawer = (
         <div>
--- src/pages/components/Navigationbar/SecondNavigationbar.tsx
+++ src/pages/components/Navigationbar/SecondNavigationbar.tsx
@@ -93,44 +93,46 @@ const SecondNavigationbar = () => {
         }
     };
 
-    const link = pages.forEach((page, index) =>
-        page.role.find((res) => isLogin.role === res) ? (
-            <ActiveLink
-                key={index}
-                href={page.path}
-                as={page.as}
-                underline="none"
-            >
-                <div className={classes.layoutDesktopAndMobile}>
-                    {page.name === "หน้าหลัก" ? (
-                        <HomeIcon
-                            className={classes.sectionMobile}
-                            titleAccess={page.name}
-                        />
-                    ) : page.name === "ประวัติการซื้อ" ? (
-                        <HistoryIcon
-                            className={classes.sectionMobile}
-                            titleAccess={page.name}
-                        />
-                    ) : page.name === "บัญชีการเงิน" ? (
-                        <PaymentIcon
-                            className={classes.sectionMobile}
-                            titleAccess={page.name}
-                        />
-                    ) : page.name === "ข้อมูลผู้ใช้" ? (
-                        <PersonIcon
-                            className={classes.sectionMobile}
-                            titleAccess={page.name}
-                        />
-                    ) : null}
+    const link = pages.map((page, index) => {
+        if (page.role.find((res) => isLogin.role === res)) {
+            return (
+                <ActiveLink
+                    key={index}
+                    href={page.path}
+                    as={page.as}
+                    underline="none"
+                >
+                    <div className={classes.layoutDesktopAndMobile}>
+                        {page.name === "หน้าหลัก" ? (
+                            <HomeIcon
+                                className={classes.sectionMobile}
+                                titleAccess={page.name}
+                            />
+                        ) : page.name === "ประวัติการซื้อ" ? (
+                            <HistoryIcon
+                                className={classes.sectionMobile}
+                                titleAccess={page.name}
+                            />
+                        ) : page.name === "บัญชีการเงิน" ? (
+                            <PaymentIcon
+                                className={classes.sectionMobile}
+                                titleAccess={page.name}
+                            />
+                        ) : page.name === "ข้อมูลผู้ใช้" ? (
+                            <PersonIcon
+                                className={classes.sectionMobile}
+                                titleAccess={page.name}
+                            />
+                        ) : null}
 
-                    <div className={classes.sectionDesktop}>
-                        &nbsp;{page.name}
+                        <div className={classes.sectionDesktop}>
+                            &nbsp;{page.name}
+                        </div>
                     </div>
-                </div>
-            </ActiveLink>
-        ) : null
-    );
+                </ActiveLink>
+            );
+        }
+    });
 
     return (
         <>
