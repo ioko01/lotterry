4f2a4aa2dea0565b6be00193f187852f7648e55b
unknown <ioko.peezaza@gmail.com>
2021-06-22 00:30:42 +0700

    2

--- src/pages/components/AddLotterryComponents/CropperLotterry.tsx
+++ src/pages/components/AddLotterryComponents/CropperLotterry.tsx
@@ -66,18 +66,6 @@ const useStyled = makeStyles((theme: Theme) =>
         input: {
             display: "none",
         },
-        fileMobile: {
-            display: "static",
-            [theme.breakpoints.up("sm")]: {
-                display: "none",
-            },
-        },
-        fileDesktop: {
-            display: "static",
-            [theme.breakpoints.down("xs")]: {
-                display: "none",
-            },
-        },
         canvas: {
             position: "absolute",
             top: 2,
@@ -90,175 +78,177 @@ const useStyled = makeStyles((theme: Theme) =>
             padding: theme.spacing(2),
             justifyContent: "space-evenly",
         },
+        gridUploadimage: {
+            borderStyle: "dashed",
+            borderWidth: "3px",
+            borderColor: theme.palette.primary.main,
+            borderRadius: ".3rem",
+            minHeight: "250px",
+        },
+        btnUploadimage: {
+            height: "100%",
+        },
+        iconUploadimage: {
+            fontSize: "50px",
+        },
     })
 );
 
-const imageUrl = "/Scan.jpg";
+const scanQrCodeWithCanvas = (canvas: HTMLCanvasElement) => {
+    try {
+        const codeReader = new BrowserMultiFormatReader();
+        const source = canvas;
+        return codeReader.decodeFromCanvas(source).getText();
+    } catch (error) {
+        return;
+    }
+};
 
-const CropperLotterry = () => {
+const showImageCanvasAndReadQrCode = (area: Area) => {
+    try {
+        const canvas = document.getElementById("canvas1") as HTMLCanvasElement;
+        const image = document.getElementById("img1") as HTMLImageElement;
+        const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx?.drawImage(
+            image,
+            area.x,
+            area.y,
+            area.width,
+            area.height,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+        return scanQrCodeWithCanvas(canvas);
+    } catch (error) {}
+};
+
+const imageUrl = "/Scan.jpg";
+type QrCodeStatusType = "SUCCESS" | "FAIL";
+
+const RenderImage = () => {
+    const classes = useStyled();
     const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
     const [zoom, setZoom] = useState(1);
     const [QRCode, setQRCode] = useState<string | null>(null);
-    const [QRCodeStatus, setQRCodeStatus] = useState<string | null>(null);
-    const classes = useStyled();
+    const [QRCodeStatus, setQRCodeStatus] = useState<QrCodeStatusType>("FAIL");
 
-    const onCropComplete = useCallback((_, croppedAreaPixels: Area) => {
-        getImage(croppedAreaPixels);
+    const onCropAreaChange = useCallback((_, croppedAreaPixels: Area) => {
+        const getQrCode = showImageCanvasAndReadQrCode(croppedAreaPixels);
+        if (getQrCode && !QRCode) {
+            setQRCode(getQrCode);
+            setQRCodeStatus("SUCCESS");
+        }
     }, []);
 
-    const scanner = () => {
-        const canvas = document.getElementById("canvas1") as HTMLCanvasElement;
-        const codeReader = new BrowserMultiFormatReader();
-        const source = canvas;
-        setQRCode(codeReader.decodeFromCanvas(source).getText());
-    };
+    return (
+        <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} sm={8} md={5} className={classes.cropGridItem}>
+                <div className={classes.app}>
+                    <div className={classes.cropContainer}>
+                        <Cropper
+                            mediaProps={{
+                                id: "img1",
+                            }}
+                            image={imageUrl}
+                            crop={crop}
+                            zoom={zoom}
+                            aspect={20 / 20}
+                            onCropChange={setCrop}
+                            onCropAreaChange={onCropAreaChange}
+                            onZoomChange={setZoom}
+                            maxZoom={20}
+                        />
+                    </div>
+                    <canvas
+                        width="100%"
+                        height="100%"
+                        id="canvas1"
+                        className={classes.canvas}
+                    />
+                    <div className={classes.controls}>
+                        <Slider
+                            value={zoom}
+                            min={1}
+                            max={20}
+                            step={0.1}
+                            aria-labelledby="Zoom"
+                            onChange={(e, zoom) => setZoom(Number(zoom))}
+                            className={classes.slider}
+                        />
+                    </div>
+                </div>
+            </Grid>
+            <Grid item sm={12} md={5} className={classes.cropGridItem}>
+                <Card className={classes.card} elevation={0}>
+                    <CardMedia
+                        id="img2"
+                        component="img"
+                        image={imageUrl}
+                        title="Paella dish"
+                    />
+                    <Box display="flex" flexDirection="column">
+                        <Box className={classes.detail}>
+                            <Typography>STATUS: {QRCodeStatus}</Typography>
+                        </Box>
+                        <Box className={classes.detail}>
+                            <Typography>NUMBER: {QRCode}</Typography>
+                        </Box>
+                    </Box>
+                </Card>
+            </Grid>
+        </Grid>
+    );
+};
 
-    const getImage = (area: Area) => {
-        try {
-            const canvas = document.getElementById(
-                "canvas1"
-            ) as HTMLCanvasElement;
-            const ctx: CanvasRenderingContext2D | null =
-                canvas.getContext("2d");
-            const image = document.getElementById("img1") as HTMLImageElement;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx?.drawImage(
-                image,
-                area.x,
-                area.y,
-                area.width,
-                area.height,
-                0,
-                0,
-                canvas.width,
-                canvas.height
-            );
+const fileselectedHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const [selectFiles, setSelectFiles] = useState<FileList | null>(null);
+    const img = document.getElementById("img1") as HTMLImageElement;
+    const img2 = document.getElementById("img2") as HTMLImageElement;
+    const files: FileList | null = e.currentTarget.files;
+    if (files) {
+        img.src = URL.createObjectURL(files[0]);
+        img2.src = URL.createObjectURL(files[0]);
+    }
+};
 
-            scanner();
-            setQRCodeStatus("SUCCESS");
-        } catch (error) {
-            if (QRCode) {
-                setQRCode(null);
-                setQRCodeStatus("FAIL");
-            }
-        }
-    };
-
-    useEffect(() => {
-        try {
-            scanner();
-            setQRCodeStatus("SUCCESS");
-        } catch (error) {
-            if (QRCode) {
-                setQRCode(null);
-                setQRCodeStatus("FAIL");
-            }
-        }
-    }, [QRCode]);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const img = document.getElementById("img1") as HTMLImageElement;
-        const img2 = document.getElementById("img2") as HTMLImageElement;
-        const file: FileList | null = e.currentTarget.files;
-        if (file) {
-            img.src = URL.createObjectURL(file[0]);
-            img2.src = URL.createObjectURL(file[0]);
-        }
-    };
+const CropperLotterry = () => {
+    const classes = useStyled();
 
     return (
         <>
-            <input
-                accept="image/*"
-                className={classes.input}
-                id="button-file"
-                type="file"
-                multiple
-                onChange={handleChange}
-            />
-            <label htmlFor="button-file" className={classes.fileMobile}>
-                <IconButton
-                    color="primary"
-                    aria-label="upload picture"
-                    component="span"
-                >
-                    <CameraAltIcon />
-                </IconButton>
-            </label>
+            <Grid container className={classes.gridUploadimage}>
+                <Grid item xs={12}>
+                    <input
+                        accept="image/*"
+                        className={classes.input}
+                        id="button-file"
+                        type="file"
+                        multiple
+                        onChange={fileselectedHandler}
+                    />
 
-            <label htmlFor="button-file" className={classes.fileDesktop}>
-                <Button
-                    color="primary"
-                    aria-label="upload picture"
-                    component="span"
-                    variant="contained"
-                >
-                    <CameraAltIcon />
-                    &nbsp;IMAGE UPLOADS
-                </Button>
-            </label>
-
-            <Grid container className={classes.gridContainer}>
-                <Grid
-                    item
-                    xs={12}
-                    sm={8}
-                    md={5}
-                    className={classes.cropGridItem}
-                >
-                    <div className={classes.app}>
-                        <div className={classes.cropContainer}>
-                            <Cropper
-                                mediaProps={{
-                                    id: "img1",
-                                }}
-                                image={imageUrl}
-                                crop={crop}
-                                zoom={zoom}
-                                aspect={20 / 20}
-                                onCropChange={setCrop}
-                                onCropComplete={onCropComplete}
-                                onCropAreaChange={onCropComplete}
-                                onZoomChange={setZoom}
-                                maxZoom={20}
+                    <label htmlFor="button-file">
+                        <Button
+                            className={classes.btnUploadimage}
+                            color="primary"
+                            aria-label="upload picture"
+                            variant="text"
+                            component="h1"
+                            fullWidth
+                        >
+                            <CameraAltIcon
+                                className={classes.iconUploadimage}
                             />
-                        </div>
-                        <canvas
-                            width="100%"
-                            height="100%"
-                            id="canvas1"
-                            className={classes.canvas}
-                        />
-                        <div className={classes.controls}>
-                            <Slider
-                                value={zoom}
-                                min={1}
-                                max={20}
-                                step={0.1}
-                                aria-labelledby="Zoom"
-                                onChange={(e, zoom) => setZoom(Number(zoom))}
-                                className={classes.slider}
-                            />
-                        </div>
-                    </div>
+                            &nbsp;IMAGES UPLOAD
+                        </Button>
+                    </label>
                 </Grid>
-                <Grid item sm={12} md={5} className={classes.cropGridItem}>
-                    <Card className={classes.card} elevation={0}>
-                        <CardMedia
-                            id="img2"
-                            component="img"
-                            image={imageUrl}
-                            title="Paella dish"
-                        />
-                        <Box display="flex" flexDirection="column">
-                            <Box className={classes.detail}>
-                                <Typography>STATUS: {QRCodeStatus}</Typography>
-                            </Box>
-                            <Box className={classes.detail}>
-                                <Typography>NUMBER: {QRCode}</Typography>
-                            </Box>
-                        </Box>
-                    </Card>
+                <Grid item xs={12}>
+                    <RenderImage />
                 </Grid>
             </Grid>
         </>
